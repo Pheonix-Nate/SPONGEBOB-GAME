23dc73120d00a6a37cabdf5f358514596d3843e4
Pheonix-Nate <89256921+Pheonix-Nate@users.noreply.github.com>
2021-08-20 13:29:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,110 @@
+// var name = "Nathan Ebiti";
+// var age = "11";
+// name = 'Luca';
+// var player;
+// player = 'Henderson';
+// const team = '?'
+// alert(name);
+// funtion a(){
+//     var country = 'nigeria';
+//     alert('My name is '+name+' and i am from '+country);
+// }
+// let a = 1
+// let b = 2
+// if (a==b){
+//     alert('lol')
+// }
+// else(alert('im happy'))
+// let number1 = 5;
+// let number2 = 6;
+// // if(number1 === number2){
+// //     alert('They are the same...')
+// // }
+// // else(alert('They are not equal'))
+// if(number1 === number2){
+//     alert('they are equal');
+// }
+// else if(number1 > number2){
+//     alert(number1+' is greater than number two'+number2)
+// }
+// else{
+//     alert(`${number1} is less than ${number1}`);
+// }
+// let day1 = 'monday';
+// let day2 = 'teusday';
+// let day3 = 'wednesday';
+// let day4 = 'thursday';
+// let day5 = 'friday';
+// let day6 = 'saturday';
+// let day7 = 'sunday';
+
+// let currentDay = 'Friday'
+// if(currentDay === day1){
+//     alert('Today is Monday')
+// }
+// else if(currentDay === day2){
+//     alert('Today is Tuesday')
+// }
+// else if(currentDay === day3){
+//     alert('Today is Wednesday')
+// }
+// else if(currentDay === day4){
+//     alert('Today is Thursday')
+// }
+// else if(currentDay === day5){
+//     alert('Today is Friday')
+// }
+// else if(currentDay === day6){
+//     alert('Today is Saturday')
+// }
+// else {
+//     alert('Today is Sunday')
+// }
+
+// switch(currentDay){
+//     case 'monday':
+//     alert('Today is Monday')
+//     break;
+//     case 'tuesday':
+//     alert('Today is Tuesday')
+//     break;
+//     case 'wednesday':
+//     alert('Today is Wednesday')
+//     break; 
+//     case 'thursday':
+//     alert('Today is Thursday')
+//     break; 
+//     case 'friday':
+//     alert('Today is Friday')
+//     break;
+//     case 'saturday':
+//     alert('Today is Saturday')
+//    break;
+//    default:
+// //        alert('Today is today')
+// // }
+// let array1 = [];
+// let array2 = new Array();     
+// let array3 = [5, 6, 'name', true];
+
+// let myArray = ['luca', 50, 21];
+// myArray.push(5);
+// myArray.pop();
+// console.log(myArray.length);
+// function saySomething(){
+//     console.log('said something');
+// }
+
+// saySomething();
+
+// let sayNothing = function(){
+//     console.log('said nothing')
+// }
+// sayNothing();
+let answers = ['spongebob squarepants', 'bikini bottom', 'patrick star', 'krusty krabs']
+let button = document.getElementById('submit')
+let answer = document.getElementById('answer')
+let score = document.getElementById('score');
+let image = document.getElementById('spongebob')
+image.innerHTML = '<img src="SPONGEBOB.png" alt="spongebob-image">';
+score.innerHTML = '<h1>0</h1>';
